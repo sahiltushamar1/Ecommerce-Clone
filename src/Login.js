52a7b835dd76a4a730d0cacbc1a0a9bb348459a4
Sahil Tushamar <sahil8713@gmail.com>
2023-11-18 14:18:13 +0530

    Intermediate Modifications for Login Authentication

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import {Link} from 'react-router-dom'
+import {auth} from './firebase.js';
+import { endAt } from 'firebase/firestore';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 function Login() {
+  
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
+
+  const createUser = async (email, password) => {
+    try{
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      console.log("User Created Successfully", user);
+    }
+
+    catch(error){
+      console.log("Error creating user");
+      alert(error.message);
+    }
+  }
+  const signIn = e => {
+    e.preventDefault();
+  }
+
+  const register = e =>{
+    e.preventDefault();
+    createUser(e.email, e.password);
+  }
+
   return (
     <div className='login'>
-      <Link to='/'>
+      <Link to='/Home'>
         <img className='login__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' alt='' />
       </Link>
 
@@ -13,12 +42,12 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>Email</h5>
-          <input type='email' />
+          <input type='email' value={email} onChange={e => setEmail(e.target.value)} />
           
           <h5>Password</h5>
-          <input type='password' />
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
 
-          <button className='login__signInButton'>Sign in</button>
+          <button className='login__signInButton' type='submit' onClick={signIn} >Sign in</button>
         </form>
 
         <p>
@@ -26,7 +55,7 @@ function Login() {
           project of Amazon-Clone. Please see our privacy notice, cookies notice and ad notice.
         </p>
 
-        <button className='login__registerButton'>Create an Account</button>
+        <button className='login__registerButton' onClick={register}>Create an Account</button>
       </div>
     </div>
   )
